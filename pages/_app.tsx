8943b8c47bb32d8a86d6cd1de5cb5cf3de0e9e5d
VitorCosta <Costa-Vitor-Fernandes>
2022-08-15 19:30:51 -0300

    removes context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
 import '../styles/globals.css'
-import {SetStateAction, useState, Dispatch} from "react"
 import type { AppProps } from 'next/app'
-import {TokenContext} from './context'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   
-  const [token,setToken] = useState<string>("no token here yet")
+ 
 
-  return (<TokenContext.Provider value={{token,setToken}}>
+  return (
   <Component {...pageProps} />
-  </TokenContext.Provider>
+  
   )
 
 }
